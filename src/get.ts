0e024fffa8e4af6eee8d8516f04093f67e1763f2
toxa16 <toxa16@gmail.com>
2018-02-07 18:35:41 +0200

    setting endpoints' service property

--- src/get.ts
+++ src/get.ts
@@ -4,11 +4,19 @@ import endpointRegister from './endpoint-register';
 
 export default function Get(route: string) {
   return (target: any, propertyName: string): void => {
+    let service: Function;
+    let serviceName: string;
+    if (target instanceof Function) {
+      service = target;
+    } else {
+      service = target.constructor;
+    }
     const endpoint: Endpoint = {
       method: HttpMethod.GET,
       route,
       propertyName,
       serviceName: target.name || target.constructor.name,
+      service: service,
     }
     endpointRegister.push(endpoint);
   }
